95b131b3e5db18072a6e3967a75b85aefe273a5c
startaiyo <startaiyo0104@gmail.com>
2025-01-12 21:54:50 +0900

    Initial Commit

---
+++ Panform/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Panform
+//
+//  Created by Shotaro Doi on 2025/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
